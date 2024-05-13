a0ab956ff56e3ce2acc4c472c522e500ba81d001
cesar <cesar@DESKTOP-DB5N9I7>
2024-05-13 07:51:37 -0600

    Agregar archivos de proyecto.

---
+++ InicioDeSesion/Models/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InicioDeSesion.Models
+{
+    public class Usuario
+    {
+        public int IDusuario { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+
+
+        public string ConfirmarClave { get; set; }
+
+    }
+}
---
+++ InicioDeSesion/permisos/ValidarSesion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InicioDeSesion.permisos
+{
+    public class ValidarSesion : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+
+            if (HttpContext.Current.Session["usuario"] == null)
+            {
+
+                filterContext.Result = new RedirectResult("~/Acceso/Login");
+            }
+
+            base.OnActionExecuted(filterContext);       
+        }
+    }
+}
